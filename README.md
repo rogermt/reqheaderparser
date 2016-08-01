d90048898bc48d0b72dfb11050404647f9857cbf
rogermt <roger_taylor@ymail.com>
2016-08-01 22:03:50 +0000

    first code release

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # reqheaderparser
+
+Objective: Build a full stack JavaScript app that is functionally 
+similar to this: https://cryptic-ridge-9197.herokuapp.com/api/whoami/ and deploy it to Heroku.
+
+User Story: I can get the IP address, language and operating system for my browser.
--- server.js
+++ server.js
@@ -9,9 +9,26 @@ var port = process.env.PORT || 8080;
 const CONCURRENCY = process.env.WEB_CONCURRENCY || 1;
 
 
+var getHeaders = function(ipAddress, language, software){
+    return{
+        ipaddress: ipAddress || null,
+        language:  language || null,
+        software: software || null
+    }
+}
 
-
+/* API return the IP address, language and operating system
+   of the client browser 
+   1. req.connection.remoteAddress for IP Address
+   2. req.headers["accept-language"] for Language
+   3. req.headers['user-agent']) for Software
+*/
+app.get('/api/whoami', function(req, res) {
+    res.json(getHeaders( req.headers['x-forwarded-for'] || req.connection.remoteAddress
+                           ,(req.headers["accept-language"]).toString().split(',')[0]  
+                           ,/\([^)]*\)/.exec(req.headers['user-agent'])[0].slice(1,-1)  ))
+})
 
 app.listen(port, function() {
     console.log('app is running on http://localhost:' + port);
-});
+})
